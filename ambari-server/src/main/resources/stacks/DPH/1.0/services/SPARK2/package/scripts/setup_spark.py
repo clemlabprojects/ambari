e3aa4032c686b68fd4fc19be874efd91c2a68346
Lucas Bakalian <lucas.bakalian@gmail.com>
2020-05-07 13:27:11 +0200

    Spark2: remove check SPARK16 version for custom actions

--- ambari-server/src/main/resources/stacks/DPH/1.0/services/SPARK2/package/scripts/setup_spark.py
+++ ambari-server/src/main/resources/stacks/DPH/1.0/services/SPARK2/package/scripts/setup_spark.py
@@ -142,7 +142,7 @@ def setup_spark(env, type, upgrade_type = None, action = None):
   if effective_version:
     effective_version = format_stack_version(effective_version)
 
-  if params.spark_thrift_fairscheduler_content and effective_version and check_stack_feature(StackFeature.SPARK_16PLUS, effective_version):
+  if params.spark_thrift_fairscheduler_content and effective_version :
     # create spark-thrift-fairscheduler.xml
     File(os.path.join(params.spark_conf,"spark-thrift-fairscheduler.xml"),
       owner=params.spark_user,
