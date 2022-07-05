6a348bf2c3aa7273ec23922cefa7695319920b73
Lucas Bakalian <lucas.bakalian@gmail.com>
2022-07-05 14:46:47 +0200

    bin.generate_repo.sh generate repo with target os

--- bin/generate_repo.sh
+++ bin/generate_repo.sh
@@ -2,7 +2,7 @@
 cat > $REPO_TARGET_FILE <<EOF
 [ambari-clemlabs-2.7.6.0-$BUILD_NUMBER]
 name=Ambari Clemlab's release
-baseurl=https://clemlabs.s3.eu-west-3.amazonaws.com/centos7/ambari-release/2.7.6.0-$BUILD_NUMBER/rpms/
+baseurl=https://clemlabs.s3.eu-west-3.amazonaws.com/$OS_TARGET/ambari-release/2.7.6.0-$BUILD_NUMBER/rpms/
 enabled=1
 gpgkey=https://clemlabs.s3.eu-west-3.amazonaws.com/RPM-GPG-KEY-Jenkins
 gpgcheck=1
