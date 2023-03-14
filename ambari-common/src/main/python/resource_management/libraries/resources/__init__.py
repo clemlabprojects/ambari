15d4b8a9c5e4f2004e1c2078b3a36dca796ea86c
Lucas Bakalian <lucas.bakalian@gmail.com>
2023-03-14 19:36:36 +0100

    AMBARI-25231 Replace deprecated hadoop commands

--- ambari-common/src/main/python/resource_management/libraries/resources/__init__.py
+++ ambari-common/src/main/python/resource_management/libraries/resources/__init__.py
@@ -21,7 +21,6 @@ Ambari Agent
 """
 
 from resource_management.libraries.resources.execute_hadoop import *
-from resource_management.libraries.resources.execute_hdfs import *
 from resource_management.libraries.resources.template_config import *
 from resource_management.libraries.resources.xml_config import *
 from resource_management.libraries.resources.properties_file import *
