82ad1327764522e5576e8cb5704a13c3d5a5a96d
Lucas Bakalian <lucas.bakalian@gmail.com>
2023-03-14 20:27:40 +0100

    AMBARI-25158: Dashboard is unable to load . Common console error : SEVERE TypeError: widgetGroups is undefined

--- ambari-web/app/views/main/dashboard/widgets.js
+++ ambari-web/app/views/main/dashboard/widgets.js
@@ -171,7 +171,6 @@ App.MainDashboardWidgetsView = Em.View.extend(App.Persist, App.LocalStorage, App
     }
     this.loadWidgetsSettings().complete(() => {
       this.get('widgetGroupsDeferred').done(() => {
-        if (this.get('isDestroyed')) return;
         this.checkServicesChange();
         this.renderWidgets();
         this.set('isDataLoaded', true);
