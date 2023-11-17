214eb201f96b914f22d66e958b572844b100b83e
Lucas Bakalian <lucas.bakalian@gmail.com>
2023-11-17 13:28:45 +0100

    AMBARI-12 (clemlab): add AMBARI-26000: Make Ambari supports Python3 environment - ambari agent fix

--- ambari-agent/src/main/python/ambari_agent/AmbariAgent.py
+++ ambari-agent/src/main/python/ambari_agent/AmbariAgent.py
@@ -29,7 +29,7 @@ if "PYTHON_BIN" in os.environ:
   AGENT_SCRIPT = os.path.join(os.environ["PYTHON_BIN"],"site-packages/ambari_agent/main.py")
 else:
   AGENT_SCRIPT = "/usr/lib/ambari-agent/lib/ambari_agent/main.py"
-if "AMBARI_PID_DIR" in os.environ):
+if "AMBARI_PID_DIR" in os.environ:
   AGENT_PID_FILE = os.path.join(os.environ["AMBARI_PID_DIR"],"ambari-agent.pid")
 else:
   AGENT_PID_FILE = "/var/run/ambari-agent/ambari-agent.pid"
