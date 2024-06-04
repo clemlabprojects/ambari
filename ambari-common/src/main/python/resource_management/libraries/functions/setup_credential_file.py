c3f94553584ce60fd30812e80d419b92acac7fc6
Lucas BAKALIAN <lucas.bakalian@gmail.com>
2024-06-04 08:41:01 +0000

    AMBARI-141 (clemlab): Fix ssl setup credential script where cmd does not exists

--- ambari-common/src/main/python/resource_management/libraries/functions/setup_credential_file.py
+++ ambari-common/src/main/python/resource_management/libraries/functions/setup_credential_file.py
@@ -34,6 +34,8 @@ from resource_management.core.utils import PasswordString
 from resource_management.libraries.script.script import Script
 from resource_management.libraries.functions.format import format
 from resource_management.libraries.functions.default import default
+from resource_management.core.shell import as_user
+from resource_management.core import shell
 
 def setup_credential_file(java_home, hadoop_bin_override,
                         credential_path, credential_owner, credential_group,
@@ -52,21 +54,37 @@ def setup_credential_file(java_home, hadoop_bin_override,
   if passwords is None:
     Logger.info("skipping credential file creation")
   else:
-
-    file_to_delete = credential_path.split(separator)[1]
-    Logger.info("Deleting existing provider")
-    rm_cmd = ('rm', '-f', file_to_delete)
-    Execute(rm_cmd, environment=cmd_env, logoutput=True, sudo=True)
-
+    # file_to_delete = credential_path.split(separator)[1]
+    # Logger.info("Deleting existing provider")
+    # rm_cmd = ('rm', '-f', file_to_delete)
+    # Execute(rm_cmd, environment=cmd_env, logoutput=True, sudo=True)
+    # logic update to verify alias existence and using credential api to modify/update password
     for password in passwords:
-      Logger.info("Adding entry \'{password.alias}\' in credential file \'{credential_path}\'")
-      cmd = (hadoop_bin, 'credential', 'create', password['alias'], '-value', PasswordString(password['value']), '-provider', credential_path)
-      Execute(cmd, environment=cmd_env, logoutput=True, sudo=True)
-      count = count+1
-    Logger.info("Credential Provider file {credential_path} successfully created")
-
-
-
-    
-        
-  
+      need_to_create = False
+      # check that the password exists first
+      Logger.info("Checking if entry \'{password.alias}\' exists in credential file \'{credential_path}\'")
+      exists_cmd = format('{hadoop_bin} credential list -provider {credential_path}')
+      code, output = shell.call(exists_cmd, timeout=5)
+      if code == 0:
+        if '{password.alias}' in output:
+          Logger.info("Entry \'{password.alias}\' exists in credential file \'{credential_path}\'")
+          Logger.info("checking if it matches")
+          check_match_cmd = '{0} credential check -provider {1} -value {2} -provider {3}'.format(hadoop_bin, password['alias'], PasswordString(password['value']), credential_path)
+          code, output = shell.call(check_match_cmd, timeout=5)
+          if code == 0 and 'Password match success' in output:
+            Logger.info("Password alias matches. Skipping...")
+          else:
+            Logger.info("Deleting alias before create")
+            delete_cmd = (hadoop_bin, 'credential', 'delete', password['alias'] , '-provider', credential_path)
+            Execute(delete_cmd, environment=cmd_env, logoutput=True, sudo=True)
+            Logger.info("Alias {password.alias} deleted")
+            need_to_create = True
+        else:
+          need_to_create = True
+      else:
+        need_to_create = True
+      if need_to_create:
+        Logger.info("Creating alias for entry \'{password.alias}\'")
+        create_cmd = (hadoop_bin, 'credential', 'create', password['alias'], '-value', PasswordString(password['value']), '-provider', credential_path)
+        Execute(create_cmd, environment=cmd_env, logoutput=True, sudo=True)
+        Logger.info("Alias {password.alias} created")
