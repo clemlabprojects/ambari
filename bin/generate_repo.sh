4c91b372b69c041e40ab02297a5817a3f37e44d7
Lucas BAKALIAN <lucas.bakalian@gmail.com>
2024-06-20 17:56:30 +0000

    Prepare 2.7.11.0.0 on RPM and DEB release

--- bin/generate_repo.sh
+++ bin/generate_repo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cat > $REPO_TARGET_FILE <<EOF
-[ambari-clemlabs-2.7.9.0.0-$BUILD_NUMBER]
+[ambari-clemlabs-2.7.11.0.0-$BUILD_NUMBER]
 name=Ambari Clemlab's release
-baseurl=https://clemlabs.s3.eu-west-3.amazonaws.com/$OS_TARGET/ambari-release/2.7.9.0.0-$BUILD_NUMBER/rpms/
+baseurl=https://clemlabs.s3.eu-west-3.amazonaws.com/$OS_TARGET/ambari-release/2.7.11.0.0-$BUILD_NUMBER/rpms/
 enabled=1
 gpgkey=https://clemlabs.s3.eu-west-3.amazonaws.com/RPM-GPG-KEY-Jenkins
 gpgcheck=1
