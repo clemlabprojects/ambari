51cbb7b7757f232225f44c74c4d207f03db7ec3f
Lucas BAKALIAN <lucas.bakalian@gmail.com>
2024-07-27 00:55:57 +0000

    parametrize repo creation jenkins key

--- bin/generate_repo.sh
+++ bin/generate_repo.sh
@@ -4,6 +4,6 @@ cat > $REPO_TARGET_FILE <<EOF
 name=Ambari Clemlab's release
 baseurl=https://clemlabs.s3.eu-west-3.amazonaws.com/$OS_TARGET/ambari-release/2.7.11.0.0-$BUILD_NUMBER/rpms/
 enabled=1
-gpgkey=https://clemlabs.s3.eu-west-3.amazonaws.com/RPM-GPG-KEY-Jenkins
+gpgkey=https://clemlabs.s3.eu-west-3.amazonaws.com/$RPM_GPG_KEY
 gpgcheck=1
 EOF
