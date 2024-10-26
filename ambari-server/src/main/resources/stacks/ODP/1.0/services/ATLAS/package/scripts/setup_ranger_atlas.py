2528a9d68cac91331332a94c213c98cb577b6000
Lucas BAKALIAN <lucas.bakalian@gmail.com>
2024-10-26 05:07:47 +0000

    AMBARI-187 (clemlab): Authorize Atlas service to access Kafka notification with Ranger Kafka Plugin policies

--- ambari-server/src/main/resources/stacks/ODP/1.0/services/ATLAS/package/scripts/setup_ranger_atlas.py
+++ ambari-server/src/main/resources/stacks/ODP/1.0/services/ATLAS/package/scripts/setup_ranger_atlas.py
@@ -16,6 +16,7 @@ limitations under the License.
 """
 from resource_management.core.logger import Logger
 from resource_management.libraries.functions.setup_ranger_plugin_xml import setup_configuration_file_for_required_plugins
+from resource_management.libraries.functions import ranger_functions_v2
 
 def setup_ranger_atlas(upgrade_type=None):
   import params
@@ -57,6 +58,20 @@ def setup_ranger_atlas(upgrade_type=None):
           Logger.info('Ranger KMS is not ssl enabled, skipping ssl-client for hdfs audits.')
       except Exception as err:
         Logger.exception("Audit directory creation in HDFS for ATLAS Ranger plugin failed with error:\n{0}".format(err))
+
+    ranger_admin_v2_obj = ranger_functions_v2.RangeradminV2(url = params.policymgr_mgr_url, skip_if_rangeradmin_down = False)
+    if params.rangranger_kafka_plugin_enabled:
+      Logger.info("Creating Ranger Kafka Notification Topics Users")
+      # Atlas hook notification publishers (hive, nifi, hbase)
+      for ranger_policy_user in params.atlas_hook_publishers:
+        ranger_admin_v2_obj.create_policy_user(params.ranger_admin_username, params.ranger_admin_password, ranger_policy_user)
+      # Ranger Tag Sync
+      ranger_admin_v2_obj.create_policy_user(params.ranger_admin_username, params.ranger_admin_password, rangertagsync_user)
+
+      # Kafka Topics related notifications
+      for ranger_policy in params.ranger_atlas_kafka_policies:
+        Logger.info("Creating Ranger Policy: {0}".format(ranger_policy.name))
+        ranger_admin_v2_obj.create_policy_urllib2(ranger_policy, params.ranger_admin_username, params.ranger_admin_password)
 
     # cred_lib_path_override  = format('{params.stack_root}/{params.stack_version}/ranger-atlas-plugin/libext/li/*')
     setup_ranger_plugin('atlas-server', 'atlas',None,
