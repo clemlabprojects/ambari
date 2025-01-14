1fb6eb0e9daca4819baed8d28188c3d5df1474b3
Lucas BAKALIAN <lucas.bakalian@gmail.com>
2025-01-14 05:36:35 +0000

    rallow only python3 ambari upgrade

--- ambari-server/src/main/resources/stacks/ODP/1.0/services/RANGER/package/scripts/setup_ranger_xml.py
+++ ambari-server/src/main/resources/stacks/ODP/1.0/services/RANGER/package/scripts/setup_ranger_xml.py
@@ -276,12 +276,12 @@ def setup_ranger_db(stack_version=None):
     Logger.info('Separate DBA property not set. Assuming Ranger DB and DB User exists!')
 
   # update db_setup.py with python2/3 compatibility for ODP 1.2.2.0 version as ambari can run in python 3 during the upgrade from ODP 1.2.2.0 -> 1.2.4.0
-  if params.ranger_compatibility_python2_python3:
+  # if params.ranger_compatibility_python2_python3:
 
-    File(format("{ranger_home}/db_setup.py"),
-       content=StaticFile('db_setup.py'),
-       mode=0o755
-    )
+  #   File(format("{ranger_home}/db_setup.py"),
+  #      content=StaticFile('db_setup.py'),
+  #      mode=0o755
+  #   )
 
   db_setup = format("ambari-python-wrap {ranger_home}/db_setup.py")
   Execute(db_setup,
