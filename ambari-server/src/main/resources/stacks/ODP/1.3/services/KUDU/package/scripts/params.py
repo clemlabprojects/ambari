06d8ee4b54f77801de29573ed7856eb553092b63
Lucas BAKALIAN <lucas.bakalian@gmail.com>
2026-03-13 13:24:19 +0000

    update kudu run dir

--- ambari-server/src/main/resources/stacks/ODP/1.3/services/KUDU/package/scripts/params.py
+++ ambari-server/src/main/resources/stacks/ODP/1.3/services/KUDU/package/scripts/params.py
@@ -48,7 +48,7 @@ kudu_group = kudu_env['kudu_group']
 kudu_conf_dir = kudu_env['kudu_conf_dir']
 kudu_log_dir = kudu_env['kudu_log_dir']
 kudu_run_dir = kudu_env['kudu_run_dir']
-ranger_kudu_receiver_fifo_dir = default('/configurations/kudu-env/ranger_kudu_receiver_fifo_dir', kudu_run_dir)
+ranger_kudu_receiver_fifo_dir = default('/configurations/kudu-env/ranger_kudu_receiver_fifo_dir', '/var/lib/kudu')
 kudu_enable_tls = str(default('/configurations/kudu-env/kudu_enable_tls', 'false')).lower() == 'true'
 kudu_tls_cert_file = default('/configurations/kudu-env/kudu_tls_cert_file', '/etc/kudu/conf/tls/kudu-server.crt')
 kudu_tls_private_key_file = default('/configurations/kudu-env/kudu_tls_private_key_file', '/etc/kudu/conf/tls/kudu-server.key')
