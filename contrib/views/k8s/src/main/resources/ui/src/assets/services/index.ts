6fcb325e70b01efc7b5aaa529bfa99f21dc26d63
Lucas BAKALIAN <lucas.bakalian@gmail.com>
2026-04-21 14:44:16 +0000

    k8s-view: add icons

---
+++ contrib/views/k8s/src/main/resources/ui/src/assets/services/index.ts
@@ -0,0 +1,11 @@
+import trinoIcon from './trino.svg';
+import supersetIcon from './superset.svg';
+import gitlabIcon from './gitlab.svg';
+import sqlAssistantIcon from './sql-assistant.svg';
+
+export const SERVICE_ICONS: Record<string, string> = {
+  TRINO: trinoIcon,
+  SUPERSET: supersetIcon,
+  GITLAB: gitlabIcon,
+  'SQL-ASSISTANT': sqlAssistantIcon,
+};
