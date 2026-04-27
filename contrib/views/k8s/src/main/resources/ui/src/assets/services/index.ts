d4fd2f32342d6b858f3caad203b7019ebed2bd24
Lucas BAKALIAN <lucas.bakalian@gmail.com>
2026-04-27 08:52:48 +0000

    AMABRI-410 (clemlab): initial commit for openmedata in KDPS

--- contrib/views/k8s/src/main/resources/ui/src/assets/services/index.ts
+++ contrib/views/k8s/src/main/resources/ui/src/assets/services/index.ts
@@ -2,10 +2,12 @@ import trinoIcon from './trino.svg';
 import supersetIcon from './superset.svg';
 import gitlabIcon from './gitlab.svg';
 import sqlAssistantIcon from './sql-assistant.svg';
+import openmetadataIcon from './openmetadata.svg';
 
 export const SERVICE_ICONS: Record<string, string> = {
   TRINO: trinoIcon,
   SUPERSET: supersetIcon,
   GITLAB: gitlabIcon,
   'SQL-ASSISTANT': sqlAssistantIcon,
+  OPENMETADATA: openmetadataIcon,
 };
